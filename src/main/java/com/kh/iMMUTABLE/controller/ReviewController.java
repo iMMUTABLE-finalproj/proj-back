be6a456492c5079b6ddfa6ea13b357fbbb26996d
LeeSoYoung <258735@naver.com>
2023-07-14 13:43:03 +0900

    0714 review 수정

--- src/main/java/com/kh/iMMUTABLE/controller/ReviewController.java
+++ src/main/java/com/kh/iMMUTABLE/controller/ReviewController.java
@@ -50,8 +50,22 @@ public class ReviewController {
     // 제품 별 리뷰 불러오기
     @GetMapping("/viewReview")
     public ResponseEntity<List<ReviewDto>> viewReview (@RequestParam String productName) {
-        System.out.println("리뷰 상품이름 : " + productName);
         List<ReviewDto> result = reviewService.viewReview(productName);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    // 내가 쓴 리뷰 불러오기
+    @GetMapping("/myReview")
+    public ResponseEntity<List<ReviewDto>> myReview (@RequestParam String id) {
+        List<ReviewDto> result = reviewService.myReview(id);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    // 내가 쓴 리뷰 삭제
+    @PostMapping("/deleteReview")
+    public ResponseEntity<Boolean> deleteReview (@RequestBody Map<String, String> deleteReview) {
+        String reviewId = deleteReview.get("reviewId");
+        boolean result = reviewService.deleteReview(reviewId);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
